aeea41d1ad8fa2cab0eb9c52c2274396b2df3a58
ChoHyunWoo <gusdndlek12@naver.com>
2025-07-07 15:45:23 +0900

    feat : 반환 타입이 맞지 않는 문제

--- src/ex1/ObjectBox.java
+++ src/ex1/ObjectBox.java
@@ -2,5 +2,13 @@ package ex1;
 
 public class ObjectBox {
 
+    private Object value;
 
+    public void set(Object object) {
+        this.value = object;
+    }
+
+    public Object get() {
+        return value;
+    }
 }
